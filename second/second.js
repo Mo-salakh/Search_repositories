ea523ea33ab5243ea5cd920d88a37f412c6eb884
Mo-salakh <mokhmadsalakh.salamov@gmail.com>
2024-07-26 15:24:26 +0300

    90%

--- second/second.js
+++ second/second.js
@@ -1,9 +1,8 @@
 let input = document.querySelector('#getPost');
 let repositoriesList = document.querySelector('.repositories_list');
-let errorMessage = document.querySelector('.error-message');
+
 
 async function getPosts(api) {
-
     if (!api) {
         repositoriesList.innerHTML = '';
         return;
@@ -11,27 +10,54 @@ async function getPosts(api) {
 
     try {
         let response = await fetch(`https://api.github.com/search/repositories?q=${api}`);
-        let repository = await response.json();
+        
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
 
+        let repository = await response.json();
         repositoriesList.innerHTML = '';
 
-        let fiveRepositories = [];
-        for (let i = 0; i < 5; i++) {
-            if (i < repository.items.length) {
-                fiveRepositories.push(repository.items[i]);
-            }
-        }
+        let fiveRepositories = repository.items.slice(0, 5);
 
-        for (let repository of fiveRepositories) {
+        console.log(fiveRepositories);
+
+        for (let repo of fiveRepositories) {
             let repositoriesListItem = document.createElement('li');
             repositoriesListItem.classList.add('repositories_item');
-            repositoriesListItem.textContent = repository.name;
+            repositoriesListItem.textContent = repo.name;
+
+            let closeIcContainer = document.createElement('div');
+            closeIcContainer.classList.add('close_ic');
+            let closeIc = document.createElement('i');
+            closeIc.classList.add('fa', 'fa-times');
+            closeIcContainer.appendChild(closeIc);
+
+            repositoriesListItem.appendChild(closeIcContainer);
             repositoriesList.appendChild(repositoriesListItem);
         }
-    } catch (error) {
-        console.error(error.message);
+
+    } 
+    catch (error) {
+        console.error('Ошибка при получении репозиториев:', error.message);
     }
 }
+
+
+repositoriesList.addEventListener('click', e => {
+    if (e.target.classList.contains('fa-times') || 
+        (e.target.parentElement && e.target.parentElement.classList.contains('close_ic'))) {
+        
+        const listItem = e.target.closest('.repositories_item');
+        
+        if (listItem) {
+            listItem.hidden = true;
+        }
+
+        
+    }
+});
+
 
 function debounce(fun, delay = 500) {
     let timeout;
